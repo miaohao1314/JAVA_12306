7c6511e0517f09f1813e750867ceaac59394cfbd
miaohao <>
2024-06-12 18:24:51 +0800

    修改pom文件依赖

---
+++ next12306/sync/src/main/java/com/next/sync/SyncApplication.java
@@ -0,0 +1,13 @@
+package com.next.sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncApplication.class, args);
+	}
+
+}
---
+++ next12306/sync/src/test/java/com/next/sync/SyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

520a311657faa78deb816f4222dd0f85df590c42
miaohao <>
2024-06-13 18:41:55 +0800

    利用generator.xml生成SysUser

---
+++ next12306/generator/src/com/next/common/dao/SysUserMapper.java
@@ -0,0 +1,17 @@
+package com.next.common.dao;
+
+import com.next.common.model.SysUser;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+}

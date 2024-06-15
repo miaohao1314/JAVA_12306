abcb6856f00e069e6d8bd032e4fc5d10078382cb
miaohao <>
2024-06-15 10:59:32 +0800

    配置数据源，common包中添加yml文件

--- next12306/backend/src/main/java/com/next/backend/BackendApplication.java
+++ next12306/backend/src/main/java/com/next/backend/BackendApplication.java
@@ -1,9 +1,14 @@
 package com.next.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@EnableTransactionManagement // 开启注解事务管理，等同于xml配置<tx:annotation-driven />
 public class BackendApplication {
 
     public static void main(String[] args) {
